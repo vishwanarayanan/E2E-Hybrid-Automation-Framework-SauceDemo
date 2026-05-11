8f188d12b1baa8b50b182b5a8ed507caaae9d7c2
vishwa narayanan <68842395+vishwanarayanan@users.noreply.github.com>
2026-05-11 23:57:32 +0530

    Replaced config-based login with Excel-driven DataProvider using TestNG to enable scalable and reusable data-driven test execution

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.testng.annotations.Test;
 
 import base.BaseDriver;
+import dataproviders.LoginDataProvider;
 import pages.LoginPage;
 
 public class LoginTest extends BaseDriver{
@@ -12,13 +13,15 @@ public class LoginTest extends BaseDriver{
 		System.out.println("Title:"+ getDriver().getTitle());
 	}
 	
-	@Test
-	public void verifyLogin() {
+	@Test(dataProvider ="loginData",
+			dataProviderClass =LoginDataProvider.class)
+	public void verifyLogin(String username,
+            String password) {
 	
 	LoginPage loginPage = new LoginPage(getDriver());
 	
-	loginPage.login(config.getUsername(), config.getPassword());
-	System.out.println("Login attempted using config data");
-	
+	loginPage.login(username, password);
+	//System.out.println("Login attempted using config data");
+	System.out.println("Login attempted using: "  + username);
 	}
 }
